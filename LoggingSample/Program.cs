1301d721132196095b1df8485284582c22fa9df3
m-abbass <modar_abbass@yahoo.com>
2022-04-11 05:05:40 +0300

    first commit

---
+++ LoggingSample/Program.cs
@@ -0,0 +1,41 @@
+using LoggingSample;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Logging.ClearProviders();
+var config = new ColoredConsoleLoggerConfiguration
+{
+  LogLevel = LogLevel.Information,
+  Color = ConsoleColor.Red
+};
+builder.Logging.AddProvider(new ColoredConsoleLoggerProvider(config));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
